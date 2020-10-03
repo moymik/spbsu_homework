8403887e3010d84b8e49adcb7b817747e2ed91ae
Artyom <6artemkim@gmail.com>
2020-10-03 22:41:21 +0300

    Try to add proj

---
+++ main tasks/task_C/cmake-build-debug/CMakeFiles/task_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_C.dir/main.cpp.obj"
+  "libtask_C.dll.a"
+  "task_C.exe"
+  "task_C.exe.manifest"
+  "task_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
