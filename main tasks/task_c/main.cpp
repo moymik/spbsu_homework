fbba498f8af42a063df40b9129968020c9e648c9
Artyom <6artemkim@gmail.com>
2020-10-05 15:14:32 +0300

    Dynamic solution

--- main tasks/task_c/main.cpp
+++ main tasks/task_c/main.cpp
@@ -2,12 +2,46 @@
 #include <string.h>
 
 using namespace std;
+/*
+ * P(n,k)=
+ * {
+ * P(n, k-1) + P(n - k, k)     (k <= n)
+ * P(n, n)  (k>n)
+ * 0    (n != 0, k = 0)
+ * 1    (n = 0, k = 0)
+ * }
+ */
+unsigned long long countPartitions(int n, int k) {
+    unsigned long long **counters = new unsigned long long *[n + 1];
+    for (int i = 0; i < n + 1; ++i)
+        counters[i] = new unsigned long long[k + 1];
+    for (int i = 0; i < n + 1; ++i) {
+        for (int j = 0; j < k + 1; ++j) {
+            counters[i][j] = 0;
+        }
+    }
 
-int countPartitions(int n, int k) {
-    if (k > n) return countPartitions(n, n);
-    if (k == 0 && n == 0) return 1;
-    if (n != 0 && k == 0) return 0;
-    return countPartitions(n - k, k) + countPartitions(n, k - 1);
+    for (int i = 0; i < n + 1; ++i) {
+        for (int j = 0; j < k + 1; ++j) {
+            if (j == 0) {
+                if (i == 0) {
+                    counters[i][j] = 1;
+                }
+            } else {
+                if (j > i) {
+                    counters[i][j] = counters[i][i];
+                } else {
+                    counters[i][j] = counters[i - j][j] + counters[i][j - 1];
+                }
+            }
+        }
+    }
+    unsigned long long result;
+    result = counters[n][k];
+    for (int i = 0; i < n + 1; ++i)
+        delete[] counters[i];
+    delete[] counters;
+    return result;
 };
 
 int main() {
@@ -16,5 +50,7 @@ int main() {
     cin >> n;
     cout << "Input k:" << endl;
     cin >> k;
-    cout << "Number of partitions = " << countPartitions(n,k);
+
+
+    cout << "Number of partitions = " << countPartitions(n, k);
 }
